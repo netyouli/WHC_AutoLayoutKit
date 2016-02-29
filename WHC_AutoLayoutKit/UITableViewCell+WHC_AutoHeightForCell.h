5faa4b02bb48fc90b1afb556a56c611f355d41e7
吴海超 <712641411@qq.com>
2016-02-29 16:45:34 +0800

    添加注释

--- WHC_AutoLayoutKit/UITableViewCell+WHC_AutoHeightForCell.h
+++ WHC_AutoLayoutKit/UITableViewCell+WHC_AutoHeightForCell.h
@@ -9,19 +9,23 @@
 #import <UIKit/UIKit.h>
 
 @interface UITableViewCell (WHC_AutoHeightForCell)
-
+/// cell最底部视图
 @property (nonatomic , strong) UIView * whc_CellBottomView;
+/// cell最底部视图集合
 @property (nonatomic , strong) NSArray * whc_CellBottomViews;
+/// cell最底部视图与cell底部的间隙
 @property (nonatomic , assign) CGFloat  whc_CellBottomOffset;
 
-
+/// 自动计算cell高度
 + (CGFloat)whc_CellHeightForIndexPath:(NSIndexPath *)indexPath tableView:(UITableView *)tableView;
 
 @end
 
 @interface UITableView (WHC_CacheCellHeight)
 
+/// 缓存cell高度字典
 @property (nonatomic , strong) NSMutableDictionary * whc_CacheHeightDictionary;
+/// cell宽度
 @property (nonatomic , strong) NSNumber * whc_CellWidth;
 
 @end
