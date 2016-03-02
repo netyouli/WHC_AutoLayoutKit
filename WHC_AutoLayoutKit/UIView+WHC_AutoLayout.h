c4fa83d05673d76b0027ee31c346f1a7c252d049
吴海超 <712641411@qq.com>
2016-03-02 14:00:37 +0800

    WHC_AutoLayoutKit 2.0 新升级

--- WHC_AutoLayoutKit/UIView+WHC_AutoLayout.h
+++ WHC_AutoLayoutKit/UIView+WHC_AutoLayout.h
@@ -6,8 +6,16 @@
 //  Copyright © 2016年 吴海超. All rights reserved.
 //
 
+/*
+ *  qq:712641411
+ *  开发作者: 吴海超(WHC)
+ *  iOS技术交流群:302157745
+ *  gitHub:https://github.com/netyouli/WHC_AutoLayoutExample
+ */
+
 #import <UIKit/UIKit.h>
 #import "UITableViewCell+WHC_AutoHeightForCell.h"
+
 
 typedef NS_OPTIONS(NSUInteger, WHC_LayoutOrientationOptions) {
     /// 垂直布局
